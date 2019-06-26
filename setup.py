2e7bcd1d5431a07e4e7d7015c3375e0ccbcf0d98
ydarma <yves.darmaillac@gmail.com>
2019-06-26 18:27:52 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='flowclus',
+    version='',
+    packages=['oc', 'flowsim', 'assembly'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    package_requires = [
+        'requests',
+        'bottle',
+        'Cython',
+        'numpy',
+        'scipy',
+        'POT',
+    ],
+    description=''
+)
